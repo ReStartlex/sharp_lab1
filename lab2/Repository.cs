fa75426e10d9a0d9ed38bdedcd3dbfe9c48eff11
Alexey <dlyauchebitpgu@gmail.com>
2023-05-25 09:58:32 +0300

    no message

---
+++ lab2/Repository.cs
@@ -0,0 +1,13 @@
+namespace sharpi_lab2
+{
+    public class Repository
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
